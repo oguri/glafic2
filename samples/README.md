ef703494756fb97b2f7a07b21ea41e14b455199d
Masamune Oguri <masamune.oguri@ipmu.jp>
2021-03-23 08:20:04 +0900

    samples added

---
+++ samples/README.md
@@ -0,0 +1,30 @@
+# sample files for glafic ver. 2
+
+Here are a few examples of how the code works. Note that these examples may require some input files which are also located here - so please run them right in this directory! Note that the example input files are updated (including .dat and .fits files) so they are not exactly same as those in version 1.
+
+1. example.input
+ This is same as the one you can generate by glafic -d. Compute lensing properties and images for a compound (nfwpot+sie) system. The lenses are located at different redshifts to show its multi lens plane capability. Check output fits files to figure out the lens system. Also use plot_point.py and check an output PDF file to figure out how the code calculates critical curves and solves lens equations for point sources. 
+
+2. extend.input
+ Fit `observed' arc images to derive the best-fit lens model. 
+
+3. point.input
+ Fitting a standard quad lens system with sie+shear. The best-fit Hubble constant is also derived from time delay measurements.  Try both chi2_splane = 0 (image plane chi^2) and = 1 (source plane chi^2) to see the difference of them.
+
+4. clugal.input
+ An example of cluster modeling that consists of a NFW halo and a number of member galaxies modeled by truncated isothermal ellipsoid (pseudo-Jaffe, actually). Run plot_point.py to check how the code resolves critical curves of small clumps.
+
+5. srcs.input
+ An example of the extended source `srcs'. In the example, both lensed extended images and PSFs are produced. You can also check the effect of different PSFs and obs parameters (gain etc) by modifying parameters for `psf' in the input file.
+
+6. quadhost.input
+ More complicated fitting with extended sources. This is an example of fitting quadruply imaged quasar lens system including its host galaxy component. Fitting lens, extended sources, and PSF components simultaneously.  
+
+7. multilens_point.input
+ Fitting for multi lens planes with 3 SIS lenses at different redshifts. Additional point source at higher redshift. Assume that positions of all the 3 SIS lenses are observed and therefore constrained. The code tries to fit redshifts of 2 background SIS lenses as well.
+
+8. multilens_extend.input
+ Similar to multilens_point.input, but fitting of extended sources. The foreground galaxy is also added. 
+
+last modified: 03/18/2021 (based on version 2.0)
+
---
+++ samples/plot_point.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter, FormatStrFormatter
+from matplotlib.ticker import StrMethodFormatter, NullFormatter
+
+mpl.rcParams['font.family'] = 'sans-serif'
+mpl.rcParams['pdf.fonttype'] = 42
+mpl.rcParams['ps.fonttype'] = 42
+mpl.rcParams['font.size'] = 20
+mpl.rcParams['legend.handlelength'] = 3.0
+mpl.rcParams['axes.linewidth'] = 1.0
+mpl.rcParams['xtick.direction'] = 'in'
+mpl.rcParams['xtick.major.width'] = 1.0
+mpl.rcParams['xtick.minor.width'] = 1.0
+mpl.rcParams['xtick.major.size'] = 5.0
+mpl.rcParams['xtick.minor.size'] = 3.0
+mpl.rcParams['xtick.top'] = True
+mpl.rcParams['ytick.direction'] = 'in'
+mpl.rcParams['ytick.major.width'] = 1.0
+mpl.rcParams['ytick.minor.width'] = 1.0
+mpl.rcParams['ytick.major.size'] = 5.0
+mpl.rcParams['ytick.minor.size'] = 3.0
+mpl.rcParams['ytick.right'] = True
+
+plt.rcParams['ps.useafm'] = True
+plt.rcParams['pdf.use14corefonts'] = True
+plt.rcParams['text.usetex'] = True
+plt.rcParams['text.latex.preamble'] = '\\usepackage{sfmath}'
+
+rfile = 'out_crit.dat'
+data = np.loadtxt(rfile, comments = '#')
+
+ix1 = data[: ,0]
+iy1 = data[: ,1]
+sx1 = data[: ,2]
+sy1 = data[: ,3]
+ix2 = data[: ,4]
+iy2 = data[: ,5]
+sx2 = data[: ,6]
+sy2 = data[: ,7]
+
+rfile = 'out_mesh.dat'
+data = np.loadtxt(rfile, comments = '#')
+
+ix3 = data[: ,0]
+iy3 = data[: ,1]
+sx3 = data[: ,2]
+sy3 = data[: ,3]
+ix4 = data[: ,4]
+iy4 = data[: ,5]
+sx4 = data[: ,6]
+sy4 = data[: ,7]
+
+rfile = 'out_point.dat'
+data = np.loadtxt(rfile, comments = '#')
+
+sxx = data[0, 2]
+syy = data[0, 3]
+ixx = data[1: ,0]
+iyy = data[1: ,1]
+
+plt.figure(figsize = (8, 16))
+
+plt.subplot(2, 1, 1)
+
+xmin = -30
+xmax = 30
+ymin = -30
+ymax = 30
+
+plt.plot([ix3, ix4], [iy3, iy4], '-', color = 'grey', zorder = 0, lw = 0.5)
+plt.plot([ix1, ix2], [iy1, iy2], '-', color = 'blue', zorder = 1)
+
+plt.scatter(ixx, iyy, s = 30, marker = '^', color = 'red', zorder = 2)
+
+ax = plt.gca()
+plt.xlim([xmin, xmax])
+plt.ylim([ymin, ymax])
+
+plt.title('image plane') 
+plt.xlabel('$\\theta_1$ [arcsec]')
+plt.ylabel('$\\theta_2$ [arcsec]')
+plt.subplots_adjust(top = 0.95)
+plt.subplots_adjust(bottom = 0.15)
+plt.subplots_adjust(left = 0.15)
+
+plt.subplot(2, 1, 2)
+
+xmin = -15
+xmax = 15
+ymin = -15
+ymax = 15
+
+plt.plot([sx3, sx4], [sy3, sy4], '-', color = 'grey', zorder = 0, lw = 0.5)
+plt.plot([sx1, sx2], [sy1, sy2], '-', color = 'blue', zorder = 1)
+
+plt.scatter(sxx, syy, s = 30, marker = '^', color = 'red', zorder = 2)
+
+ax = plt.gca()
+plt.xlim([xmin, xmax])
+plt.ylim([ymin, ymax])
+
+plt.title('source plane') 
+plt.xlabel('$\\beta_1$ [arcsec]')
+plt.ylabel('$\\beta_2$ [arcsec]')
+plt.subplots_adjust(top = 0.95)
+plt.subplots_adjust(bottom = 0.15)
+plt.subplots_adjust(left = 0.15)
+
+ofile = 'plot_point'
+
+#plt.savefig(ofile + '.png', dpi = 150, transparent = True)
+plt.savefig(ofile + '.pdf', bbox_inches = 'tight')

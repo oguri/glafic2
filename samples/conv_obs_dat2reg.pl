3b6fda9c74fae1eb1724a779d17cdbde171899b8
Masamune Oguri <masamune.oguri@chiba-u.jp>
2022-07-23 11:41:39 +0900

    sample scripts updated

---
+++ samples/conv_obs_dat2reg.pl
@@ -0,0 +1,108 @@
+#!/usr/bin/perl
+#
+# convert obs file for glafic to ds9 region file
+#
+
+# size of circle in arcsec (for WCS)
+$rr=0.4;
+# color of cirle
+$clr='magenta';
+
+if((($#ARGV+1)!=1)&&(($#ARGV+1)!=2)){
+    die "usage:\n > conv_obs_dat2reg.pl model_hoge.input [obs_hoge.dat]\n    [obs_hoge.dat] is optional\n";
+}
+
+## check model_hoge.input file
+open(IN,"$ARGV[0]") || die "File Not Found\n";
+
+$x0="";
+$y0="";
+while(<IN>){
+    if(substr($_,0,1)  ne '#'){
+        @data=split;
+        
+        $com=$data[0];
+	if($com eq 'prefix'){ $prefix=$data[1]; }
+        if($com eq 'wcs_ra0'){  $x0=$data[1]; }
+        if($com eq 'wcs_dec0'){ $y0=$data[1]; }
+	if($com eq 'readobs_point'){ $ofile=$data[1]; }
+    }
+}
+
+close(IN);
+
+if($x0 =~ /[0-9]/){
+    printf "use WCS (ra0: %s , dec0 %s)\n",$x0,$y0; 
+} else {
+    printf "use relative coordinates\n";
+}
+
+## read obs file, write reg file
+if(($#ARGV+1)==1){
+    $fc=$ofile;
+} else {
+    $fc=$ARGV[1];
+}
+
+open(IN,"$fc") || die "File Not Found\n";
+
+$f1="reg_".substr($fc, rindex($fc, '/') + 1);;
+
+$f1 =~ s/.dat/.reg/g;
+
+printf("input obs file    : %s\n",$fc);
+printf("output region file: %s\n",$f1);
+
+open(OUT,">$f1") || die "Error: File Not Found\n";
+
+print OUT "# Region file format: DS9 version 4.1\n";
+print OUT "global color=green font=\"helvetica 8 normal roman\"\n";
+if($x0 =~ /[0-9]/){
+    print OUT "fk5\n";
+} else {
+    print OUT "physical\n";
+}
+
+open(IN,"$fc") || die "File Not Found\n";
+
+while(<IN>){
+    if(substr($_,0,1)  ne '#'){
+	@data=split;
+	if(/[0-9]/){
+	    $idid=$data[0];
+	    $nimg=$data[1];
+	    
+	    for($i=1;$i<=$nimg;$i++){
+		$_=<IN>;
+		@data=split;
+
+		if(substr($_,0,1)  ne '#'){
+		    $dr=$data[0];
+		    $dd=$data[1];
+		    
+		    if($x0 =~ /[0-9]/){
+			$dec=(3.141592/180.0)*$y0;
+			$ys=$y0+$dd/3600.0;
+			$xs=$x0-$dr/(3600.0*cos($dec));
+		    } else {
+			$xs=$dr;
+			$ys=$dd;
+		    }
+		    $to=sprintf("%s.%s",$idid,$i);
+		    
+		    if($x0 =~ /[0-9]/){
+			print OUT "circle($xs,$ys,$rr\") # width=2 text={$to} color=$clr\n";
+		    } else {
+			print OUT "circle($xs,$ys,$rr) # width=2 text={$to} color=$clr\n";
+		    }
+		} else {
+		    $i=$i-1;
+		}
+	    }
+	}
+    }
+}
+
+close(IN);
+close(OUT);
+

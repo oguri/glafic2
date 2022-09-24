a5d2d7ac46871380e0cbfb196258d7769cd1f253
Masamune Oguri <masamune.oguri@chiba-u.jp>
2022-09-24 23:18:14 +0900

    setup.py added

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import subprocess
+
+from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def build_extensions():
+    subprocess.run(
+        "make clean; make python",
+        shell = True,
+        check = True,
+    )
+
+
+def make_cmd_class(base):
+    class CmdClass(base):
+        def run(self):
+            build_extensions()
+            super().run()
+
+    return CmdClass
+
+setup(
+    name = 'glafic',
+    version = '2.1.3',
+    author = 'Masamune Oguri',
+    description = 'Libary for gravitational lensing analyses',
+    url = 'https://github.com/oguri/glafic2',
+    packages = find_packages(where = 'python'),
+    package_dir = {'': 'python'},
+    python_requires = '>=3',
+    package_data = {'glafic': ['*.so']},
+    cmdclass = {
+       'build_py': make_cmd_class(build_py)
+    },
+)
